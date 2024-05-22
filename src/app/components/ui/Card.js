7ca2cebb4663a30627ba320832580d847093c9e4
itsNiazi <niazitareqmejl@gmail.com>
2024-05-22 18:40:00 +0200

    moved around ternary operator for a temporary image render

--- src/app/components/ui/Card.js
+++ src/app/components/ui/Card.js
@@ -3,8 +3,14 @@ import getImageUrl from "@/app/utils/getImageUrl";
 export default function Card({ name, rating, imageId, onFavorite }) {
   return (
     <article>
-      {/* Check ?getImageUrl : insert temp img path */}
-      <img src={getImageUrl(200, imageId)} alt={name} />
+      <img
+        src={
+          imageId
+            ? getImageUrl(200, imageId)
+            : "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
+        }
+        alt={name}
+      />
       <h2>{name}</h2>
       <p>{rating}</p>
       <button onClick={onFavorite}>Favorite</button>
