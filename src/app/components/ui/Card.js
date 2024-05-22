dfd5df563c250104c0ad7ccba6caa74419914a82
bdrn <100045933+bdrn@users.noreply.github.com>
2024-05-22 20:10:57 +0200

    added placeholder image if no image is found

--- src/app/components/ui/Card.js
+++ src/app/components/ui/Card.js
@@ -1,10 +1,18 @@
 import getImageUrl from "@/app/utils/getImageUrl";
 
+
+const ImageNotFound = "/images/imageNotFound.jpg";
+
 export default function Card({ name, rating, imageId, onFavorite }) {
+  const imageUrl = imageId ? getImageUrl(200, imageId) : null;
+
   return (
     <article>
-      {/* Check ?getImageUrl : insert temp img path */}
-      <img src={getImageUrl(200, imageId)} alt={name} />
+      {imageUrl ? (
+        <img src={imageUrl} alt={name} />
+      ) : (
+        <img src={ImageNotFound} alt="Image Not Found" />
+      )}
       <h2>{name}</h2>
       <p>{rating}</p>
       <button onClick={onFavorite}>Favorite</button>
