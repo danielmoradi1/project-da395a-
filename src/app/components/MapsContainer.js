cd595ad8ae3373a0bbefd52d325019c2859321a2
bdrn <100045933+bdrn@users.noreply.github.com>
2024-05-22 17:20:54 +0200

    added error handling if api key is missing

--- src/app/components/MapsContainer.js
+++ src/app/components/MapsContainer.js
@@ -2,15 +2,21 @@ import { APIProvider, Map } from "@vis.gl/react-google-maps";
 import PinMarker from "./ui/PinMarker";
 
 export default function MapsContainer() {
+  const apiKey = process.env.NEXT_PUBLIC_MAPS_KEY;
+
+  if (!apiKey) {
+    return <div>Error: Google Maps API key is missing</div>;
+  }
+
   return (
     <>
-      <APIProvider apiKey={process.env.NEXT_PUBLIC_MAPS_KEY}>
+      <APIProvider apiKey={apiKey}>
         <Map
           mapId="3573efe3f3b75066"
           style={{ width: "25vw", height: "50vh" }}
           defaultCenter={{ lat: 22.54992, lng: 0 }}
           defaultZoom={3}
-          gestureHandling={"greedy"}
+          gestureHandling="greedy"
           disableDefaultUI={true}
         >
           <PinMarker locations={{ lat: -33.8567844, lng: 151.213108 }} />
