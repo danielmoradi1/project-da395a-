64bf4e774751c72f46f2106642515c0276b16512
itsNiazi <niazitareqmejl@gmail.com>
2024-05-25 12:51:57 +0200

    Tried changing Pin color, rounding map corners & Random place button with confetti onClick

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,3 @@
-// "use client";
-// import { useGlobalContext } from "./context/globalContext";
-// import SearchForm from "./components/SearchForm";
-// import CardList from "./components/CardList";
-// import MapsContainer from "./components/MapsContainer";
-// import { clearLocalStorage } from "./utils/localStorage";
-
-// export default function Application() {
-//   const { favoritedPlaces, addFavoritedPlace, places, addPlaces } =
-//     useGlobalContext();
-//   // clearLocalStorage();
-
-//   return (
-//     //Bryt in till component?
-//     <div className="section">
-//       <div className="title-container">
-//         <h1 className="title">Find your favorite places</h1>
-//       </div>
-
-//       <SearchForm addPlaces={addPlaces} />
-//       <MapsContainer locations={favoritedPlaces} />
-//       <CardList places={places} onFavorite={addFavoritedPlace} />
-//     </div>
-//   );
-// }
-
 "use client";
 import { useState } from "react";
 import { useGlobalContext } from "./context/globalContext";
